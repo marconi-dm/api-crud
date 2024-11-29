eb091e49c26f1e866942fc58e6f86eb2425faba4
marconi-dm <marconidamascenomd@gmail.com>
2024-11-29 15:35:06 -0300

    update cors

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import customers from './customerRoutes.js';
 import users from './userRoutes.js';
+import cors from 'cors';
 /*
 Definição das Rotas Principais da Aplicação
 Este módulo funciona como um "barrel", centralizando e configurando as rotas principais da aplicação.
@@ -12,10 +13,15 @@ Descrição Geral:
 - O middleware `express.json()` é utilizado para garantir que o corpo das requisições seja interpretado como JSON.
 */
 const routes = (app) => {
+    app.use(
+        cors({
+            origin:'https://basic-crm-gamma.vercel.app'
+        })
+    )
     // Define a rota base "/"
     app.route("/").get((req, res) => res.status(200).send("API Running"));
     // Registra o middleware para interpretar JSON e as rotas de clientes
-    app.use(express.json(), customers, users);
+    app.use(express.json(), customers, users,);
 };
 
 
